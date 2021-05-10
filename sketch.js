98cfcd48e455234dba0876164b91d08dd7f9e4bd
ojasvi2828 <77545304+ojasvi2828@users.noreply.github.com>
2021-05-10 01:24:44 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ var backgroundimg;
 function preload() {
     //load the images here
     backgroundimg = loadImage("images/garden.png");
-    tomImg1= loadImage("images/tomOne.png"); 
+    tomImg1= loadAnimation("images/tomOne.png"); 
     tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png"); 
-    tomImg3= loadImage("images/tomFour.png"); 
-    jerryImg1=loadImage("images/jerryOne.png");
+    tomImg3= loadAnimation("images/tomFour.png"); 
+    jerryImg1=loadAnimation("images/jerryOne.png");
  jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-      jerryImg3=loadImage("images/jerryFour.png");
+      jerryImg3=loadAnimation("images/jerryFour.png");
 }
 
 function setup(){
@@ -16,20 +16,22 @@ function setup(){
    
     //create tom and jerry sprites here
     mouse = createSprite(150,660,20,20);
-    mouse.addImage(jerryImg1);
+    mouse.addAnimation(jerryImg1);
     mouse.scale = 0.2;
     cat = createSprite(690,680,50,50);
-    cat.addImage(tomImg1);
+    cat.addAnimation(tomImg1);
     cat.scale = 0.2;
 }
 
 function draw() {
     background(backgroundimg);
-    if (cat.X  -  mouse.X < (cat.width - mouse.width) /2 ) {
+    if (cat.x  -  mouse.x < (cat.width - mouse.width) /2 ) {
       //Write condition here to evalute if tom and jerry collide
         cat.velocityX = 0;
 cat.addAnimation("catLastImage",tomImg3);
-cat.changeAnimation("catLastImage");
+cat.changeAnimation("catLastImage",tomImg3);
+mouse.addAnimation("images/jerryTwo.png","images/jerryThree.png",jerryImg2);
+mouse.changeAnimation("images/jerryTwo.png","images/jerryThree.png",jerryImg2);
     }
     
   
